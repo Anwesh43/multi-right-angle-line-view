68e85cbf8687b20e742afa80e8c2353b6cd2cd33
Anwesh43 <anweshthecool0@gmail.com>
2020-03-01 18:15:11 +0530

    adding Animaor class

--- multirightangleview/src/main/java/com/anwesh/uiprojects/multirightangleview/MultiRightAngleLineView.kt
+++ multirightangleview/src/main/java/com/anwesh/uiprojects/multirightangleview/MultiRightAngleLineView.kt
@@ -91,4 +91,32 @@ class MultiRightAngleLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }

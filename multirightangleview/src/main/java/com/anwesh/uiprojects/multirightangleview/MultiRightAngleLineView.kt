ed6d00a8054a1f5b66318e19a664db75197ef829
Anwesh43 <anweshthecool0@gmail.com>
2020-03-01 17:52:06 +0530

    adding constants and libraries

--- multirightangleview/src/main/java/com/anwesh/uiprojects/multirightangleview/MultiRightAngleLineView.kt
+++ multirightangleview/src/main/java/com/anwesh/uiprojects/multirightangleview/MultiRightAngleLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.multirightangleview
 /**
  * Created by anweshmishra on 01/03/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 5
+val parts : Int = 2
+val scGap : Float = 0.02f / (lines * parts)
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#1A237E")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20

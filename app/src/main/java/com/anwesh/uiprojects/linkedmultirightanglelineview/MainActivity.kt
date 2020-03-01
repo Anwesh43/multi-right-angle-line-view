a1125d43931fed5b728c90d6969d853659ff0604
Anwesh43 <anweshthecool0@gmail.com>
2020-03-01 18:51:43 +0530

    adding view and using fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedmultirightanglelineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmultirightanglelineview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedmultirightanglelineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.multirightangleview.MultiRightAngleLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MultiRightAngleLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }

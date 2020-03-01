2cabfb6f5ca2862b4f911d5555c0dcbeaefee0f7
Anwesh43 <anweshthecool0@gmail.com>
2020-03-01 18:24:02 +0530

    adding MultiRightAngleLine class

--- multirightangleview/src/main/java/com/anwesh/uiprojects/multirightangleview/MultiRightAngleLineView.kt
+++ multirightangleview/src/main/java/com/anwesh/uiprojects/multirightangleview/MultiRightAngleLineView.kt
@@ -160,4 +160,27 @@ class MultiRightAngleLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class MultiRightAngleLine(var i : Int) {
+
+        private var curr : MRALNode = MRALNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }

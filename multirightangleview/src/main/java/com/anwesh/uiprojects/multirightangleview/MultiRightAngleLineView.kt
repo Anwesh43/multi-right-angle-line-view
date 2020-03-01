90560109706119a9b0fde39c4808bb89aa1f7d0c
Anwesh43 <anweshthecool0@gmail.com>
2020-03-01 17:53:57 +0530

    adding Float and Int extension methods

--- multirightangleview/src/main/java/com/anwesh/uiprojects/multirightangleview/MultiRightAngleLineView.kt
+++ multirightangleview/src/main/java/com/anwesh/uiprojects/multirightangleview/MultiRightAngleLineView.kt
@@ -20,3 +20,8 @@ val strokeFactor : Int = 90
 val foreColor : Int = Color.parseColor("#1A237E")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
